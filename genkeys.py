d3f67e96a2003cc8847245aac8007eff233d3ef9
Radu Potop <radu@wooptoo.com>
2023-07-31 11:26:34 +0100

    Tweak templates

--- genkeys.py
+++ genkeys.py
@@ -1,23 +1,30 @@
-import yaml
-import wgtools
-from yaml.loader import SafeLoader
 from pathlib import Path
-from models import HostModel
+
+import wgtools
+import yaml
+from yaml.loader import SafeLoader
+
+from models import HostModel, InterfaceModel, PeerModel
 
 interfaces_yaml = Path('interfaces.yaml')
+
 
 def genkeypair() -> wgtools.Keypair:
     return wgtools.keypair()
 
+
 def genpsk():
     return wgtools.genpsk()
+
 
 def loadyaml():
     return yaml.load(interfaces_yaml.read_bytes(), Loader=SafeLoader)
 
+
 def parseyaml():
     cfgdata = loadyaml()
-    for ifname, ifdata in cfgdata['Hosts'].items():
-        print(HostModel.parse_obj(ifdata))
+    for ifname, ifdata in cfgdata['Machines'].items():
+        print(ifname, HostModel.parse_obj(ifdata))
+
 
 parseyaml()
--- models.py
+++ models.py
@@ -3,19 +3,22 @@ from pydantic import BaseModel
 
 class InterfaceModel(BaseModel):
     Address: str
+    Netmask: int
     ListenPort: int
-    PrivateKey: str
+    PrivateKey: str | None
 
 
 class PeerModel(BaseModel):
     AllowedIPs: str
     ## AllowedIPs: 0.0.0.0/0, ::/0 # Forward all traffic through this server
+    Netmask: int
     Endpoint: str | None
+    Port: int
     PersistentKeepalive: int | None
-    PublicKey: str
-    PresharedKey: str
+    PublicKey: str | None
+    PresharedKey: str | None
 
 
 class HostModel(BaseModel):
-    Interface: InterfaceModel
-    Peer: PeerModel
+    Interface: InterfaceModel | None
+    Peer: PeerModel | None
